0e38c6a2f3ff7682c4ad579268638ae9fd598585
Zhen-hao <zhenhaolee@gmail.com>
2023-10-22 21:51:28 +0200

    update nix files

--- default.nix
+++ default.nix
@@ -2,6 +2,14 @@
 # this file originates from SBTix
 { pkgs ? import <nixpkgs> {}
 , cleanSource ? pkgs.lib.cleanSource
+, gitignore ? (let
+                 src = pkgs.fetchFromGitHub { 
+                   owner = "hercules-ci";
+                   repo = "gitignore.nix";
+                   rev = "9e21c80adf67ebcb077d75bd5e7d724d21eeafd6";
+                   sha256 = "sha256:vky6VPK1n1od6vXbqzOXnekrQpTL4hbPAwUhT5J9c9E=";
+                 };
+               in import src { inherit (pkgs) lib; })
 }:
 
 let
@@ -9,7 +17,7 @@ let
 in
   sbtix.buildSbtLibrary {
     name = "root";
-    src = cleanSource ./.;
+    src = cleanSource (gitignore.gitignoreSource ./.);
     #sbtixBuildInputs = pkgs.callPackage ./sbtix-build-inputs.nix {};
     repo = [
       (import ./repo.nix)
